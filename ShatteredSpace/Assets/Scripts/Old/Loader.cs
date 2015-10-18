a1d400722dd3d7425a4c00f88b698317889996dc
matthew9671 <yixiuz@andrew.cmu.edu>
2015-10-18 17:12:14 -0400

    This is what we have right now and it almost works\!

---
+++ ShatteredSpace/Assets/Scripts/Old/Loader.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+namespace Completed{
+public class Loader : MonoBehaviour {
+		public GameObject gameManager;          //GameManager prefab to instantiate.
+		public GameObject soundManager;         //SoundManager prefab to instantiate.
+
+		//I have no idea why we need this loader...
+		//But I guess it has something to do with scene transitions so we better not touch it!
+		
+		void Awake ()
+		{
+			//Check if a GameManager has already been assigned to static variable GameManager.instance or if it's still null
+			if (gameManager.activeInHierarchy==false)
+				
+				//Instantiate gameManager prefab
+				Instantiate(gameManager);
+			
+			//Check if a SoundManager has already been assigned to static variable GameManager.instance or if it's still null
+			//if (soundManager.instance == null)
+				
+				//Instantiate SoundManager prefab
+			//	Instantiate(soundManager);
+		}
+}
+}
---
+++ ShatteredSpace/Assets/Scripts/Old/upgradeController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class upgradeController : MonoBehaviour {
+
+	public upgradeStats[] upgrades;
+
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void install(int upgradeIndex){
+
+	}
+}
