8063a3da93fa6ebb3b18ed8e42156e6924fd3ac8
matthew9671 <yixiuz@andrew.cmu.edu>
2015-10-21 22:04:26 -0400

    Where did all the copies come from!?

--- blaster.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-
-public class blaster : weapon { 
-	// The most basic weapon
-	// Need to add recoil push after upgrades
-	public blaster():base("blaster","blaster",5,5,1){
-	}
-}
