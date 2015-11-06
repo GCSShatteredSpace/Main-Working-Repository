c8c2174720f141f988d2b19ffb3970b63480d4ed
MeenaGuptaIwasaki <mguptaiwasaki@hotmail.com>
2015-11-06 16:33:10 -0500

    minigun compiles and doesn't break game, but I have tested the functionality yet

---
+++ ShatteredSpace/Assets/Scripts/New/minigun.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public struct FireInstance
+{
+    public bool generatedDamage;
+    public int fireTime;
+    public Vector2 targetPosition;
+
+    public FireInstance(int fireTime, Vector2 pos)
+    {
+        this.fireTime = fireTime;
+        this.targetPosition = pos;
+        this.generatedDamage = false;
+    }
+}
+
+public class minigun : weapon
+{
+    static private int DAMAGE = 2;
+    static private int RANGE = 2;
+    static private int DELAY = 1;
+    private List<FireInstance> shots;
+    // The most basic weapon
+    // Need to add recoil push after upgrades
+    private int numDmgGenerated = 0;
+   
+
+    public minigun()
+        : base("minigun", "minigun", DAMAGE, RANGE, DELAY)
+    {
+        shots = new List<FireInstance>();
+    }
+
+    void Update()
+    {
+        if (numDmgGenerated < shots.Count)
+        {
+            for (int i = 0; i < shots.Count; i++)
+            {
+                FireInstance shot = shots[i];
+                if (tManager.getTime() == shot.fireTime && !shot.generatedDamage)
+                {
+                    print("FireTime = " + fireTime.ToString());
+                    generateDamage();
+                    shot.generatedDamage = false;
+                    numDmgGenerated = 0;
+                }
+            }
+        }
+    }
+
+    new public void fireWeapon(Vector2 pos, int time)
+    {
+        print("Weapon fired!");
+        fireTime = time + delay;
+
+        FireInstance shot = new FireInstance(fireTime, pos);
+        shots.Add(shot);
+    }
+}
