433bc0859bcced70e90c6bf4b738a4cb3673103b
MeenaGuptaIwasaki <mguptaiwasaki@hotmail.com>
2015-11-14 18:52:44 -0500

    eh... probably not working laser

--- ShatteredSpace/Assets/Scripts/New/weapons/laser.cs
+++ ShatteredSpace/Assets/Scripts/New/weapons/laser.cs
@@ -3,6 +3,30 @@ using System.Collections;
 
 public class laser : weapon { 
 	// Just a place holder
+
+	bool overheat;
+	int turnFired;
+
 	public laser():base("Laser","No delay but overheats when fired.",4,4,0,1){
 	}
+
+	void Update(){
+		base.Update();
+		if(overheat && tManager.getTurn() == turnFired + 2){
+			overheat = false;
+		}
+	}
+
+	public override void fireWeapon(Vector2 pos, int time){
+		base.fireWeapon(pos, time);
+		overheat = true;
+		turnFired = tManager.getTurn();
+	}
+
+
+	public override bool readyToFire(){
+		return base.readyToFire() && !overheat;
+	}
+
+
 }
