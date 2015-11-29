34c75bc93bd10733ec49df54bb23cf30687fa0b1
matthew9671 <yixiuz@andrew.cmu.edu>
2015-11-28 21:35:57 -0500

    Got deflector shield working; AObserved one bug with grenade

--- ShatteredSpace/Assets/Scripts/New/weapons/deflectorShield.cs
+++ ShatteredSpace/Assets/Scripts/New/weapons/deflectorShield.cs
@@ -2,7 +2,24 @@
 using System.Collections;
 
 public class deflectorShield : weapon { 
-	// Placeholder
-	public deflectorShield():base("Deflector Shield","field","Deflector Shield",3,10,0,1){
+
+	private static int maxDamage = 3;
+
+	public deflectorShield():base("Deflector Shield","field","Deflector Shield makes you immune" +
+		"to all damage less or equal to " + maxDamage.ToString(),0,0,0,isDefensive: true){
+	}
+
+	void Update(){
+		print (this.isDefeniveWeapon());
+	}
+
+	public override bool blockDamage(int amount, weapon source){
+		bool blocked = amount <= maxDamage;
+		if (blocked) {
+			print ("Reduced damage from " + amount.ToString () + " to 0!");
+		} else {
+			print ("Block failed!");
+		}
+		return blocked;
 	}
 }
